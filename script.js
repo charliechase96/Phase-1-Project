f7d475ec5449a7e97d25b1369df05e1a31099529
Charlie Gray <charliechasegray@gmail.com>
2023-03-28 08:12:20 -0700

    moved sorting functions down to a new function outside of the original renderKittens function

--- script.js
+++ script.js
@@ -9,92 +9,97 @@ document.addEventListener("DOMContentLoaded", () => {
 
     function renderKittens(kitten) {
 
-            const orangeKittens = [];
-            const blackKittens = [];
-            const whiteKittens = [];
-            const grayKittens = [];
-            const tuxedoKittens = [];
-
-            allKittens.push(kitten);
-
-            allKittens.filter(sortKittens);
-            
-            function sortKittens(kitten) {
-
-                if (kitten.color === "orange") {
-                    orangeKittens.push(kitten);
-                }
-                else if (kitten.color === "black") {
-                    blackKittens.push(kitten);
-                }
-                else if (kitten.color === "white") {
-                    whiteKittens.push(kitten);
-                }
-                else if (kitten.color === "gray") {
-                    grayKittens.push(kitten);
-                }
-                else if (kitten.color === "tuxedo") {
-                    tuxedoKittens.push(kitten);
-                }
+        
+        
+        
+        let card = document.createElement("div");
+        card.classList.add("card");
+        
+        let kittenCollection = document.getElementById("kitten-collection");
+        kittenCollection.appendChild(card);
+        
+        let kittenImg = document.createElement("img");
+        kittenImg.src = `${kitten.image}`;
+        card.appendChild(kittenImg);
+        kittenImg.classList.add("kitten-avatar");
+        kittenImg.setAttribute("id", `kitten${kitten.id}`);
+        
+        let likeButton = document.createElement("button");
+        likeButton.addEventListener("click", (event) => {
+            addLike(event, kitten);
+        })
+        
+        likeButton.classList.add("like-btn");
+        likeButton.setAttribute("id", `${kitten.id}`);
+        likeButton.textContent = EMPTY_HEART;
+        card.appendChild(likeButton);
+        
+        
+        function addLike(event, kitten) {
+            if (event.target.textContent === EMPTY_HEART) {
+                event.target.textContent = FULL_HEART;
+                renderFavoriteKitten(kitten);
             }
-            
-            // currently trying to implement this event listener to display each type of kitten ONLY, and hide the others. It is not going well.
-
-            // function displayKittensByColor() {
-            //     const kittenColor = document.getElementById("kitten-color").value;
-            //     kittenColor.addEventListener("change", (event) => {
-            //         if (kittenColor === "orange") {
-            //             document.remove(grayKittens);
-            //             document.remove(blackKittens);
-            //             document.remove(whiteKittens);
-            //             document.remove(tuxedoKittens);
-            //         }
-            //     })
-            // }
-
-            
-            
-            let card = document.createElement("div");
-            card.classList.add("card");
-            
-            let kittenCollection = document.getElementById("kitten-collection");
-            kittenCollection.appendChild(card);
-            
-            let kittenImg = document.createElement("img");
-            kittenImg.src = `${kitten.image}`;
-            card.appendChild(kittenImg);
-            kittenImg.classList.add("kitten-avatar");
-            kittenImg.setAttribute("id", `kitten${kitten.id}`);
-            
-            let likeButton = document.createElement("button");
-            likeButton.addEventListener("click", (event) => {
-                addLike(event, kitten);
-            })
-            
-            likeButton.classList.add("like-btn");
-            likeButton.setAttribute("id", `${kitten.id}`);
-            likeButton.textContent = EMPTY_HEART;
-            card.appendChild(likeButton);
-            
-            
-            function addLike(event, kitten) {
-                if (event.target.textContent === EMPTY_HEART) {
-                    event.target.textContent = FULL_HEART;
-                    renderFavoriteKitten(kitten);
-                }
-                else {
-                    event.target.textContent = EMPTY_HEART;
-                    const selectedImage = (document.getElementById(`${kitten.image}`));
-                    selectedImage.remove();
-                }
-            }
-            
-            function renderFavoriteKitten(kitten) {
-                let favoriteKittensArea = document.getElementById("favorite-kittens");
-                const image = document.createElement("img");
-                image.id = `${kitten.image}`;
-                image.src = kitten.image;
-                favoriteKittensArea.append(image);
+            else {
+                event.target.textContent = EMPTY_HEART;
+                const selectedImage = (document.getElementById(`${kitten.image}`));
+                selectedImage.remove();
             }
         }
+        
+        function renderFavoriteKitten(kitten) {
+            let favoriteKittensArea = document.getElementById("favorite-kittens");
+            const image = document.createElement("img");
+            image.id = `${kitten.image}`;
+            image.src = kitten.image;
+            favoriteKittensArea.append(image);
+        }
+    }
+    
+    function dropDownKittens(kitten) {
+    const orangeKittens = [];
+    const blackKittens = [];
+    const whiteKittens = [];
+    const grayKittens = [];
+    const tuxedoKittens = [];
+
+    allKittens.push(kitten);
+
+    allKittens.filter(sortKittens);
+    
+    function sortKittens(kitten) {
+
+        if (kitten.color === "orange") {
+            orangeKittens.push(kitten);
+        }
+        else if (kitten.color === "black") {
+            blackKittens.push(kitten);
+        }
+        else if (kitten.color === "white") {
+            whiteKittens.push(kitten);
+        }
+        else if (kitten.color === "gray") {
+            grayKittens.push(kitten);
+        }
+        else if (kitten.color === "tuxedo") {
+            tuxedoKittens.push(kitten);
+        }
+    }
+    
+    // currently trying to implement this event listener to display each type of kitten ONLY, and hide the others. It is not going well.
+
+    // function displayKittensByColor() {
+    //     const dropDownValue = document.getElementById("kitten-color").value;
+        const kittenColorDropDown = document.getElementById("kitten-color");
+        console.log(kittenColorDropDown);
+    //     kittenColor.addEventListener("change", (event) => {
+    //         if (kittenColor === "orange") {
+    //             document.remove(grayKittens);
+    //             document.remove(blackKittens);
+    //             document.remove(whiteKittens);
+    //             document.remove(tuxedoKittens);
+    //         }
+    //     })
+    // }
+    }
 })
